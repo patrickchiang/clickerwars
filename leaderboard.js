973ec2ff27589b5affe7b8467017a76c5ee29d99
patrickchiang <pchiang@uw.edu>
2014-01-17 00:47:48 -0800

    Cleanup & added login ui

--- leaderboard.js
+++ leaderboard.js
@@ -4,44 +4,56 @@
 Players = new Meteor.Collection("players");
 
 if (Meteor.isClient) {
-  Template.leaderboard.players = function () {
-    return Players.find({}, {sort: {score: -1, name: 1}});
-  };
+	
+	Accounts.ui.config({
+		passwordSignupFields: 'USERNAME_ONLY'
+	});
+	
+	Template.leaderboard.players = function() {
+		return Players.find({}, {
+			sort : {
+				score : -1,
+				name : 1
+			}
+		});
+	};
 
-  Template.leaderboard.selected_name = function () {
-    var player = Players.findOne(Session.get("selected_player"));
-    return player && player.name;
-  };
+	Template.leaderboard.selected_name = function() {
+		var player = Players.findOne(Session.get("selected_player"));
+		return player && player.name;
+	};
 
-  Template.player.selected = function () {
-    return Session.equals("selected_player", this._id) ? "selected" : '';
-  };
+	Template.player.selected = function() {
+		return Session.equals("selected_player", this._id) ? "selected" : '';
+	};
 
-  Template.leaderboard.events({
-    'click input.inc': function () {
-      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
-    }
-  });
+	Template.leaderboard.events({
+		'click input.inc' : function() {
+			Players.update(Session.get("selected_player"), {
+				$inc : {
+					score : 5
+				}
+			});
+		}
+	});
 
-  Template.player.events({
-    'click': function () {
-      Session.set("selected_player", this._id);
-    }
-  });
+	Template.player.events({
+		'click' : function() {
+			Session.set("selected_player", this._id);
+		}
+	});
 }
 
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace",
-                   "Grace Hopper",
-                   "Marie Curie",
-                   "Carl Friedrich Gauss",
-                   "Nikola Tesla",
-                   "Claude Shannon"];
-      for (var i = 0; i < names.length; i++)
-        Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
-    }
-  });
+	Meteor.startup(function() {
+		if (Players.find().count() === 0) {
+			var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie", "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+			for (var i = 0; i < names.length; i++)
+				Players.insert({
+					name : names[i],
+					score : Math.floor(Random.fraction() * 10) * 5
+				});
+		}
+	});
 }
